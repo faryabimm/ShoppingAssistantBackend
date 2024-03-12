34028a18d60d0f85feec6d14f8c9619233d5849b
Mohammadmahdi Faryabi <faryabimm@gmail.com>
2024-03-12 23:53:55 +0330

    update allowed hosts

--- shoppingAssistantBackend/settings.py
+++ shoppingAssistantBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hzf4=!pbf(m$4j=nlerc7mguysw@=%ieex794l)3cx3742x4ib
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["shopping-assistant.divar.dev", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["go.shopping-assistant.divar.dev", "127.0.0.1", "localhost"]
 
 
 # Application definition
